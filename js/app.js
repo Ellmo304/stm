af1ba5d4eed85ae98b45dfea68b2eb374937261a
Elliot Brock <ell_304@msn.com>
2017-01-15 22:10:36 +0000

    pop out nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 $(function() {
   console.log('Sabre Loaded!!!!');
+
+  function showMenu() {
+    $('#menuButton').css('visibility', 'hidden');
+    $('#navBar').css('visibility', 'visible');
+    $('#menuButton2').css('visibility', 'visible');
+  }
+
+  function closeMenu() {
+    $('#menuButton2').css('visibility', 'hidden');
+    $('#navBar').css('visibility', 'hidden');
+    $('#menuButton').css('visibility', 'visible');
+  }
+
+  $('#menuButton').on('click', showMenu);
+  $('#menuButton2').on('click', closeMenu);
+
+
+
+
+
+
+
+
+
+
+
+
 });
