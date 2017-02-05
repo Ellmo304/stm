87e258ef86155fd54e0c318f5d417e59ca1551ee
Elliot Brock <ell_304@msn.com>
2017-02-05 20:38:16 +0000

    added video embedding to videos page

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@ $(function() {
   console.log('Sabre Loaded!!!!');
 
   const $eyes = $('.eye');
+  // const $c2 = $('#c2');
 
   const $songOne = new Audio('../audio/one.mp3');
-  const $songFangs = new Audio('../audio/fangs.mp3');
+  // const $songFangs = new Audio('../audio/fangs.mp3');
+
+  const video = `<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fsabretoothmonkmusic%2Fvideos%2F1399419080092748%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>`;
+
 
   function showMenu() {
     $('#menuButton').css('visibility', 'hidden');
@@ -40,9 +44,20 @@ $(function() {
     setTimeout(function(){
       $('#splashDiv').remove();
       $('.content').append(
-      ' <img class="bannerImage" src="../images/banner.jpg" alt="bandPhoto"/>\
-      <h1 class="fadeIn">SABrE TOOTH MOnK</h1>\
-      <div id="c1" class="choice"><h2>MUsic</h2></div><div id="c2" class="choice"><h2>VidEos</h2></div><div id="c3" class="choice"><h2>Gigs</h2></div>');
+        ' <img class="bannerImage" src="../images/banner.jpg" alt="bandPhoto"/>\
+        <h1 class="fadeIn">SABrE TOOTH MOnK</h1>\
+        <div id="c1" class="choice"><h2>MUsic</h2></div><div id="c2" class="choice"><h2>VidEos</h2></div><div id="c3" class="choice"><h2>Gigs</h2></div>');
+    }, 3000);
+  });
+
+  $('body').on('click', '#c2', function() {
+    $songOne.pause();
+    $('.content').css('animation', 'fadeOutObject 3s');
+    setTimeout(function(){
+      $('.content').empty();
+      $('.content').append(
+        `<h1 class="fadeIn">SABrE TOOTH MOnK</h1>\
+        ${video}`);
     }, 3000);
   });
 
